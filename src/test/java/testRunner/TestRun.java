adccc1f1bcb6f6aa6829bbcc501b0de0426e0f51
alanliew8888 <keanoon03@gmail.com>
2022-11-25 02:04:35 +0800

    Debugging the StaleElementReferenceException in the old Cucumber file

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -2,7 +2,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "/Users/alanliew/Desktop/Selenium Java Practise/SelfSetupCucumber/Features/Learningtogether.feature",
